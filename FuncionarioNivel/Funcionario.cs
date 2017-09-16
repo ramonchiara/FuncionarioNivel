3b3fecfaf891992e8c677e8f4ca35b8d67ad72f1
Ramon Chiara <ramonchiara@gmail.com>
2017-09-16 20:01:09 -0300

    Solução do Item 3

---
+++ FuncionarioNivel/Funcionario.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace FuncionarioNivel
+{
+    public class Funcionario
+    {
+        private string nome;
+        private int nivel;
+        private double horas;
+
+        public Funcionario(string nome, int nivel, double horas)
+        {
+            this.nome = nome;
+            this.nivel = nivel;
+            this.horas = horas;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public double GetHoras()
+        {
+            return horas;
+        }
+
+        public decimal GetValor()
+        {
+            decimal valor = 0;
+
+            switch (nivel)
+            {
+                case 1: valor = 15; break;
+                case 2: valor = 18; break;
+                case 3: valor = 22; break;
+                case 4: valor = 27; break;
+                case 5: valor = 33; break;
+            }
+
+            return valor;
+        }
+
+        public int GetNivel()
+        {
+            return nivel;
+        }
+
+        public decimal GetSalario()
+        {
+            return Convert.ToDecimal(horas) * GetValor();
+        }
+
+        public void Promover()
+        {
+            if (nivel < 5)
+            {
+                nivel++;
+            }
+        }
+    }
+}
---
+++ FuncionarioNivel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FuncionarioNivel
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Nivel: ");
+            int nivel = int.Parse(Console.ReadLine());
+
+            Console.Write("Horas: ");
+            double horas = double.Parse(Console.ReadLine());
+
+            Console.Write("Promoção (s/n): ");
+            string promocao = Console.ReadLine();
+
+            Funcionario f = new Funcionario(nome, nivel, horas);
+
+            Console.WriteLine("O funcionário {0} trabalhou {1} horas por R$ {2} (Nível {3}) e deve receber R$ {4}",
+                              f.GetNome(), f.GetHoras(), f.GetValor(), f.GetNivel(), f.GetSalario());
+
+            if (promocao.ToLower() == "s") // promocao == "s" || promocao == "S"
+            {
+                f.Promover();
+
+                Console.WriteLine("O funcionário {0} teve promoção, trabalhou {1} horas por R$ {2} (Nível {3}) e o seu novo salário é de R$ {4}",
+                                  f.GetNome(), f.GetHoras(), f.GetValor(), f.GetNivel(), f.GetSalario());
+            }
+        }
+    }
+}
